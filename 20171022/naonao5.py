c1789877edceee5a04a1dfb41d0bf6b593d09518
chaochaogoogle <972689056@qq.com>
2017-10-22 23:09:30 +0800

    2017.10.22

---
+++ 20171022/naonao5.py
@@ -0,0 +1,16 @@
+import os
+
+print([d for d in os.listdir('.')])
+L = []
+for x in range(1, 11):
+    L.append(x * x)
+print(L)
+
+print([y * y for y in range(1, 11)])
+
+print([x * x for x in range(1, 11) if x % 2 == 0])
+
+print([m + n for m in 'abc' for n in 'xyz'])
+
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+print([k + '=' + v for k, v in d.items()])
