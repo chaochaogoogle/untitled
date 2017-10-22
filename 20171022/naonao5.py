2b22876eb62c4a97a5dbcfcacc3c7be9574ecdff
chaochaogoogle <972689056@qq.com>
2017-10-23 07:48:58 +0800

    2017.10.22

--- 20171022/naonao5.py
+++ 20171022/naonao5.py
@@ -12,5 +12,8 @@ print([x * x for x in range(1, 11) if x % 2 == 0])
 
 print([m + n for m in 'abc' for n in 'xyz'])
 
-d = {'x': 'A', 'y': 'B', 'z': 'C' }
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
 print([k + '=' + v for k, v in d.items()])
+
+M = ['Hello', 'World', 'IBM', 'Apple']
+print([s.lower() for s in M])
